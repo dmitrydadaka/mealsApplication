0570fc76501bf114e81cea77bb48b1de75b18ce9
Dmitry Dadaka <belpc2012@gmail.com>
2023-05-07 04:01:20 +0300

    feat: added css styles and features for meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,15 +1,38 @@
 /* import { useContext } from 'react';
 import { AppContext } from '../context'; */
 import { useGlobalContext } from '../context';
+import { BsHandThumbsUp } from 'react-icons/bs'
+
 
 const Meals = () => {
     //const context = useContext(AppContext);
-    const { meals } = useGlobalContext();
-    console.log(meals);
+    const { meals, loading } = useGlobalContext();
+
+    if( loading ){
+        return <section className='section'>
+            <h4>Loading...</h4>
+        </section>
+    }
+
+    if( meals.length < 1){
+        return <section className='section'>
+            <h4>No meals matched your search term. Please try again.</h4>
+        </section>
+    }
+
     return (
-    <section>
+    <section className='section-center'>
         {
-            meals.map(singleMeal => <h4>{singleMeal.strMeal}</h4>)
+            meals.map(singleMeal => {
+                const { idMeal, strMeal: title, strMealThumb: image} = singleMeal 
+                return <article id={idMeal} className='single-meal'>
+                    <img src={ image } alt="image" className='img'  style={{ width: '200px'}}/>
+                    <footer>
+                        <h5> { title } </h5>
+                        <button className='like-btn'> <BsHandThumbsUp /> </button>
+                    </footer>
+                </article>               
+            })
         }
     </section>
     )
--- src/context.jsx
+++ src/context.jsx
@@ -4,26 +4,33 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
 
-    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+    const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
     const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
+    const [loading, setLoading] = useState(false)
     const [meals, setMeals] = useState([])
 
 
     const fetchMeals = async (url) => {
+        setLoading(true);
         try {
             const { data } = await axios(url);
-            setMeals(data.meals);
+            if(data.meals){
+                setMeals(data.meals);
+            } else {
+                setMeals([])
+            }
         } catch (error) {
             console.log(error.response);
         }
+        setLoading(false);
     }
 
     useEffect(() => {
         fetchMeals(allMealsUrl);
     }, []);
 
-    return <AppContext.Provider value={{ meals }}>
+    return <AppContext.Provider value={{ loading, meals }}>
         {children}
     </AppContext.Provider>
 }
